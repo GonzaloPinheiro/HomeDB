6b3ab91b97bd89cfd7392c4b81c5301ca8c900d5
GonzaloPinheiro <140199326+GonzaloPinheiro@users.noreply.github.com>
2026-07-04 15:57:57 +0200

    creada migración estadísticas ventilador

---
+++ HomeDB.Infrastructure/Migrations/20260704133713_AgregadoMetricasVentilador.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace HomeDB.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AgregadoMetricasVentilador : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "fan_control_mode",
+                table: "system_metrics_entries",
+                type: "text",
+                nullable: true);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "fan_is_running",
+                table: "system_metrics_entries",
+                type: "boolean",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "fan_pwm_duty_cycle",
+                table: "system_metrics_entries",
+                type: "integer",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "fan_rpm_speed",
+                table: "system_metrics_entries",
+                type: "integer",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "fan_control_mode",
+                table: "system_metrics_entries");
+
+            migrationBuilder.DropColumn(
+                name: "fan_is_running",
+                table: "system_metrics_entries");
+
+            migrationBuilder.DropColumn(
+                name: "fan_pwm_duty_cycle",
+                table: "system_metrics_entries");
+
+            migrationBuilder.DropColumn(
+                name: "fan_rpm_speed",
+                table: "system_metrics_entries");
+        }
+    }
+}
